ae55ca4b87d15794b14a17f5c732b2faac1ef181
Ioannis <jondan97@gmail.com>
2023-05-23 09:43:26 +0300

    added iterator showcase

--- src/main/java/gr/codelearn/Main.java
+++ src/main/java/gr/codelearn/Main.java
@@ -1,7 +1,24 @@
 package gr.codelearn;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    private void iteratorShowcase(){
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+
+        Iterator<Integer> iterator = integers.iterator();
+        while(iterator.hasNext()){
+            Integer next = iterator.next();
+            System.out.println(next);
+        }
     }
 }
