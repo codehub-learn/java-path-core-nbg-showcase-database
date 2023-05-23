86134bbbf84122b79fb0f2c04a604ccc166268e6
Ioannis <jondan97@gmail.com>
2023-05-23 10:06:20 +0300

    added starting/stopping of H2 server, added loading of h2 driver

--- src/main/java/gr/codelearn/Main.java
+++ src/main/java/gr/codelearn/Main.java
@@ -1,11 +1,48 @@
 package gr.codelearn;
 
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Main {
-    public static void main(String[] args) {
 
+    Server server;
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.startH2Server();
+        main.loadJDBCDriver();
+        main.stopH2Server();
+
+    }
+
+    private void loadJDBCDriver(){
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not load driver");
+            System.exit(-1);
+        }
+    }
+
+    private void startH2Server(){
+        try {
+            System.out.println("Starting H2 Server.");
+            this.server = Server.createTcpServer();
+            server.start();
+        } catch (SQLException e) {
+            System.out.println("Could not start server");
+            System.exit(-1);
+        }
+    }
+
+    private void stopH2Server(){
+        if(server.isRunning(true)){
+            System.out.println("Stopping H2 Server.");
+            server.stop();
+        }
     }
 
     private void iteratorShowcase(){
